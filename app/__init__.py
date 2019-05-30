567a434a9695b545b5706cf324720efb20588617
joao.heckmann <joao.heckmann@senior.com.br>
2019-05-30 14:01:19 -0300

    Decyphering the code and creating file

--- app/__init__.py
+++ app/__init__.py
@@ -15,24 +15,39 @@ headers = {
 
 response = requests.request("GET", url, data=payload, headers=headers, params=querystring, verify=False)
 
-print(response)
-print('Response' + response.text)
+print('Response is:' + response.text)
 
 # Parse the response JSON as a Dictionary
 jsonResponse = json.loads(response.text)
-print(type(jsonResponse))
+
+# Get the cypher in the JSON response
+cypher = jsonResponse['numero_casas']
 
 decodedString = ""
-# Get the cypher in the JSON response
-#cypher = response.text.numero_casas 
-for letter in response.text:
+for letter in jsonResponse['cifrado']:
     if letter.isalpha():
-        decodedLetter = chr(ord(letter) + cypher)
-        decodedString = decodedString.append(decodedLetter)
+        decodedLetter = chr(ord(letter) - cypher)
+        decodedString = decodedString.__add__(decodedLetter)
     else:
-        decodedString = decodedString.append(letter)
+        decodedString = decodedString.__add__(letter)
 
-print(decodedString)
+print('Decoded string is: ' + decodedString)
+jsonResponse['decifrado'] = decodedString
+jsonResponse[]
 
-with open('answer.json', 'w') as outfile:
-	json.dump(data, outfile)
+## Use SH1 to encrypt the decoded string
+
+fileName = 'answer.json'
+with open(fileName, 'w') as outfile:
+    json.dump(jsonResponse, outfile)
+
+with open(fileName, 'rb') as f:
+    url = "https://api.codenation.dev/v1/challenge/dev-ps/submit-solution"
+    headers = {
+        'Content-Type': "multipart/form-data"
+    }
+    postResponse = requests.post(url, files={fileName: f}, headers=headers, params=querystring, verify=False)
+
+    print(postResponse)
+
+
